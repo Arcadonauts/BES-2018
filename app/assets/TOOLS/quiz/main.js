0e17dc0adc95f333db345c81278c8731f07ba9f7
Arcadonauts <33792996+Arcadonauts@users.noreply.github.com>
2020-02-19 20:04:05 +0800

    Fixed tools/chat to work with two-character names

--- app/assets/TOOLS/quiz/main.js
+++ app/assets/TOOLS/quiz/main.js
@@ -1,5 +1,39 @@
 console.log('hello world')
 
+let random = {
+	_seeded: false,
+	hash: function xmur3(str) {
+		for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
+			h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
+			h = h << 13 | h >>> 19;
+		return function() {
+			h = Math.imul(h ^ h >>> 16, 2246822507);
+			h = Math.imul(h ^ h >>> 13, 3266489909);
+			return (h ^= h >>> 16) >>> 0;
+		}
+	},
+	sfc32: function (a, b, c, d) {
+		return function() {
+		  a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
+		  var t = (a + b) | 0;
+		  a = b ^ b >>> 9;
+		  b = c + (c << 3) | 0;
+		  c = (c << 21 | c >>> 11);
+		  d = d + 1 | 0;
+		  t = t + d | 0;
+		  c = c + t | 0;
+		  return (t >>> 0) / 4294967296;
+		}
+	},
+	seed: function(str){
+		let h = this.hash('' + str)
+		this.random = this.sfc32(h(), h(), h(), h())
+	},
+	random: function(){
+		throw("Must seed random before using")
+	}
+}
+
 function simpson(f, a, b, n){
 	let dx = (b-a)/n 
 	let s = 0
